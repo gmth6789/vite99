7b29561f7fd3e8835cddf8b2e0259c39b5fcba69
gmth6789 <143313748+gmth6789@users.noreply.github.com>
2023-08-28 02:24:53 +0700

    1 binaries uploaded

--- src/App.vue
+++ src/App.vue
@@ -31,6 +31,45 @@ const currentView = computed(() => {
   <a href="#/rank">Rank</a> |
   <a href="#/profile">Profile</a> |
 
+
+<div class="flex diygw-col-24 diygw-bottom">
+					<div class="diygw-grid diygw-actions">
+						<button class="diygw-action">
+							<div class="diygw-grid-inner">
+								<div class="diygw-grid-icon diygw-avatar" style="">
+									<img mode="aspectFit" class="diygw-avatar-img" src="/static/grid1.png" />
+								</div>
+								<div class="diygw-grid-title">หน้าหลัก</div>
+							</div>
+						</button>
+						<button class="diygw-action">
+							<div class="diygw-grid-inner">
+								<div class="diygw-grid-icon diygw-avatar" style="">
+									<img mode="aspectFit" class="diygw-avatar-img" src="./static/grid2.png" />
+								</div>
+								<div class="diygw-grid-title">菜单二</div>
+							</div>
+						</button>
+						<button class="diygw-action">
+							<div class="diygw-grid-inner">
+								<div class="diygw-grid-icon diygw-avatar" style="">
+									<img mode="aspectFit" class="diygw-avatar-img" src="./static/grid3.png" />
+								</div>
+								<div class="diygw-grid-title">菜单三</div>
+							</div>
+						</button>
+						<button class="diygw-action">
+							<div class="diygw-grid-inner">
+								<div class="diygw-grid-icon diygw-avatar" style="">
+									<img mode="aspectFit" class="diygw-avatar-img" src="./static/grid4.png" />
+								</div>
+								<div class="diygw-grid-title">菜单四</div>
+							</div>
+						</button>
+					</div>
+				</div>
+
+
   <component :is="currentView" />
 </template>
 
