15f9c9c71330a1b5901117e9f6c6516157b26d0e
gmth6789 <143313748+gmth6789@users.noreply.github.com>
2023-08-28 03:01:04 +0700

    1 file modified

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,6 @@
 <template>
-  <h1>Home</h1>
+ <div class="flex flex-wrap diygw-col-0 items-stretch flex9-clz">
+					<div class="diygw-col-0 text3-clz">马上抢</div>
+					<img src="./static/sd.png" class="image-size diygw-image diygw-col-0 image-clz" mode="heightFix" />
+				</div>
 </template>
