3f1b1c2effbc920b2b913853733ad7011bdbeab6
gmth6789 <143313748+gmth6789@users.noreply.github.com>
2023-08-28 02:29:37 +0700

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -47,7 +47,7 @@ const currentView = computed(() => {
 								<div class="diygw-grid-icon diygw-avatar" style="">
 									<img mode="aspectFit" class="diygw-avatar-img" src="./static/grid2.png" />
 								</div>
-								<div class="diygw-grid-title">菜单二</div>
+								<div class="diygw-grid-title">ภารกิจ</div>
 							</div>
 						</button>
 						<button class="diygw-action">
@@ -55,7 +55,15 @@ const currentView = computed(() => {
 								<div class="diygw-grid-icon diygw-avatar" style="">
 									<img mode="aspectFit" class="diygw-avatar-img" src="./static/grid3.png" />
 								</div>
-								<div class="diygw-grid-title">菜单三</div>
+								<div class="diygw-grid-title">อันดับ</div>
+							</div>
+						</button>
+            <button class="diygw-action">
+							<div class="diygw-grid-inner">
+								<div class="diygw-grid-icon diygw-avatar" style="">
+									<img mode="aspectFit" class="diygw-avatar-img" src="./static/grid5.png" />
+								</div>
+								<div class="diygw-grid-title">แชท</div>
 							</div>
 						</button>
 						<button class="diygw-action">
@@ -63,7 +71,7 @@ const currentView = computed(() => {
 								<div class="diygw-grid-icon diygw-avatar" style="">
 									<img mode="aspectFit" class="diygw-avatar-img" src="./static/grid4.png" />
 								</div>
-								<div class="diygw-grid-title">菜单四</div>
+								<div class="diygw-grid-title">ข้อมูล</div>
 							</div>
 						</button>
 					</div>
